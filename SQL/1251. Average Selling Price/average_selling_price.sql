470df7593260ce2ad0c8987d8ba247b081c48fe1
Alejandro Cortes <alex.cortes.i10@gmail.com>
2025-01-04 18:24:53 +0800

    updated with left join, as the UnitsSold table might have not a single entry.

--- SQL/1251. Average Selling Price/average_selling_price.sql
+++ SQL/1251. Average Selling Price/average_selling_price.sql
@@ -2,7 +2,7 @@ SELECT p.product_id,
 ROUND (
     COALESCE(CAST(SUM(u.units*p.price) as FLOAT)/NULLIF(SUM(u.units),0),0), 2
 ) as average_price
-FROM Prices p JOIN UnitsSold u
+FROM Prices p LEFT JOIN UnitsSold u
 ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN p.start_date and p.end_date
 GROUP BY p.product_id
