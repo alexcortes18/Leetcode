a5c121e20dfbb131e44e91e59b992d4982cb5853
Alejandro Cortes <alex.cortes.i10@gmail.com>
2025-01-07 17:41:21 +0800

    SQL: 1484. Group Sold Products By The Date

---
+++ SQL/1484. Group Sold Products By The Date/group_sold_products_by_date.sql
@@ -0,0 +1,13 @@
+/* Write your T-SQL query statement below */
+
+WITH DistinctProducts AS(
+    SELECT DISTINCT sell_date, product
+    FROM Activities
+)
+Select 
+    sell_date, 
+    COUNT(product) AS num_sold, 
+    STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product ASC) AS products
+FROM DistinctProducts
+GROUP BY sell_date
+ORDER BY sell_date ASC
